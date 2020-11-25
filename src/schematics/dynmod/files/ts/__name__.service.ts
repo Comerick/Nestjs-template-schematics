cad313b52afa2d0e6a456a51560e905a353fdff0
Gróf Erik <erik.grof@attrecto.com>
2020-11-25 13:19:58 +0100

    Service template repository is typed

--- src/schematics/dynmod/files/ts/__name__.service.ts
+++ src/schematics/dynmod/files/ts/__name__.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { TypeOrmCrudService } from "@nestjsx/crud-typeorm";
+import { Repository } from 'typeorm';
 
 import { <%= classify(name) %>} from '../../../database/entity/generated/';
 
 @Injectable()
 export class <%= classify(name) %>Service extends TypeOrmCrudService<<%= classify(name) %>> {
-  constructor(@InjectRepository(<%= classify(name) %>) <%= lowercased(name) %>Repo) {
+  constructor(@InjectRepository(<%= classify(name) %>) <%= lowercased(name) %>Repo: Repository<<%= classify(name) %>>) {
     super(<%= lowercased(name) %>Repo);
   }
 }
