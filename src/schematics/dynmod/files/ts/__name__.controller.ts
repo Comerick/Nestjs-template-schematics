f37ed55fe7e987ca4611171dac7804cd88db7ee9
Gróf Erik <erik.grof@attrecto.com>
2020-11-25 15:19:27 +0100

    Added automatic relation join

--- src/schematics/dynmod/files/ts/__name__.controller.ts
+++ src/schematics/dynmod/files/ts/__name__.controller.ts
@@ -1,16 +1,30 @@
 import { Controller } from "@nestjs/common";
 import { ApiTags } from '@nestjs/swagger';
 import { Crud, CrudController } from "@nestjsx/crud";
+import { getMetadataArgsStorage } from 'typeorm';
 
 import { <%= classify(name) %>Service } from "./<%= lowerCase(name) %>.service";
 import { <%= classify(name) %> } from '../../../database/entity/generated/';
+import { RoleGuard } from '../../auth/guards/role.guard';
+import { Role } from '../../../common/role';
+
+const joinProps = {};
+getMetadataArgsStorage().relations.forEach((relation)=>{
+  if(relation.target == <%= classify(name) %>){
+    joinProps[relation.propertyName] = {eager: true};
+  }
+});
 
 @ApiTags('<%= classify(name) %>')
 @Crud({
   model: {
     type: <%= classify(name) %>,
   },
+  query: {
+    join: joinProps
+  }
 })
+@UseGuards(RoleGuard([Role.ADMIN]))
 @Controller("<%= dasherize(name) %>")
 export class <%= classify(name) %>Controller implements CrudController<<%= classify(name) %> > {
   constructor(public service: <%= classify(name) %>Service) {}
